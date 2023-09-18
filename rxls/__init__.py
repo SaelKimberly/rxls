5a84da805a3ef5d09dd14947372a0945da671756
SaelKimberly <57495324+SaelKimberly@users.noreply.github.com>
2023-09-18 16:20:37 +0500

    Create __init__.py

---
+++ rxls/__init__.py
@@ -0,0 +1,5 @@
+from .core import as_dataclass, cached
+from .record_enum import BIFF_ENUM, BIFF_ENUM_REVERSED
+from .record import RecordProto, SampleProto, record
+from .rels import Relationship
+from .reader.reader import xl_scan
