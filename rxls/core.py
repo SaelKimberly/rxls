d239ce9e434060a83cc8146a48430736e6f486e0
SaelKimberly <sael.kimberly@yandex.ru>
2024-01-13 14:36:18 +0500

    Update `core.py`. * remove unnecessary functions * add constants to track numba and tbb status.

--- rxls/core.py
+++ rxls/core.py
@@ -1,53 +1,59 @@
-import typing
-from collections import deque
-from struct import Struct
-
-from recordclass import as_dataclass as _as_dataclass
-from typing_extensions import dataclass_transform
+from __future__ import annotations
 
 __all__ = [
+    "POLARS_AVAILABLE",
+    "NUMBA_AVAILABLE",
+    "NUMBA_TBB_AVAILABLE",
     "cached",
     "as_dataclass",
-    "exhaust",
-    "u8_u",
-    "u8_p",
     "u4_u",
     "u4_p",
-    "u2_u",
     "u2_p",
-    "u1_u",
-    "u1_p",
-    "f8_u",
-    "f8_p",
-    "f4_u",
-    "f4_p",
     "struct_u",
     "struct_p",
     "end_decl",
     "Un",
+    "PaValue",
+    "PaArray",
 ]
+import warnings
+from struct import Struct
+from typing import TYPE_CHECKING
 
+from recordclass import as_dataclass as _as_dataclass
+from typing_extensions import (
+    Unpack as Un,
+)
+from typing_extensions import dataclass_transform
 
-if typing.TYPE_CHECKING:
-    from typing import Any, Callable, Iterable, NoReturn, Type, TypeVar
+if TYPE_CHECKING:
+    from typing import Callable, NoReturn, TypeVar
 
-    from typing_extensions import ParamSpec, TypeVarTuple
-    from typing_extensions import Unpack as Un
-
-    P = ParamSpec("P")
-    T = TypeVar("T")
-    Ts = TypeVarTuple("Ts")
+    import pyarrow as pa
+    from typing_extensions import (
+        ParamSpec,
+        TypeAlias,
+        TypeVarTuple,
+    )
 
     try:
-        from functools import cache
+        from functools import cache  # type: ignore
     except ImportError:
         from functools import lru_cache as cache
 
-    def cached(f: "Callable[P, T]") -> "Callable[P, T]":
-        raise NotImplementedError()
+    PaArray: TypeAlias = "pa.Array | pa.ChunkedArray"
+    PaValue: TypeAlias = "pa.Array | pa.ChunkedArray | pa.Scalar"
 
-    cached = cache  # type: ignore  # noqa: F811
+    PV = TypeVar("PV", pa.Scalar, pa.Array)
 
+    T = TypeVar("T")
+    P = ParamSpec("P")
+    Ts = TypeVarTuple("Ts")
+
+    def cached(f: Callable[P, T]) -> Callable[P, T]:
+        raise NotImplementedError
+
+    cached = cache  # type: ignore # noqa: F811
 else:
     try:
         from functools import cache as cached
@@ -55,9 +61,40 @@ else:
         from functools import lru_cache as cached
 
 
+try:
+    import numba
+
+    NUMBA_AVAILABLE = True
+
+    try:
+        from numba.np.ufunc import parallel
+
+        warnings.filterwarnings("ignore", ".*TBB.*", numba.errors.NumbaWarning)
+
+        parallel._check_tbb_version_compatible()
+
+        NUMBA_TBB_AVAILABLE = True
+
+    except ImportError:
+        NUMBA_TBB_AVAILABLE = False
+
+
+except ImportError:
+    NUMBA_AVAILABLE = False
+    NUMBA_TBB_AVAILABLE = False
+
+
+try:
+    import polars as pl  # noqa: F401
+
+    POLARS_AVAILABLE = True
+except ImportError:
+    POLARS_AVAILABLE = False
+
+
 @dataclass_transform()
 def as_dataclass(
-    cls: "Type[T] | None" = None,
+    cls: type[T] | None = None,
     *,
     use_dict: bool = False,
     use_weakref: bool = False,
@@ -66,11 +103,10 @@ def as_dataclass(
     mapping: bool = False,
     iterable: bool = False,
     readonly: bool = False,
-    module: "str | None" = None,
     fast_new: bool = True,
     rename: bool = False,
-    gc: bool = False
-) -> "Callable[[Type[T]], Type[T]]":
+    gc: bool = False,
+) -> Callable[[type[T]], type[T]]:
     if cls is not None:
         return _as_dataclass(
             use_dict=use_dict,
@@ -80,13 +116,12 @@ def as_dataclass(
             mapping=mapping,
             iterable=iterable,
             readonly=readonly,
-            module=module,
             fast_new=fast_new,
             rename=rename,
             gc=gc,
-        )(cls)
+        )(cls)  # type: ignore
 
-    def wrapper(cls: "Type[T]") -> "Type[T]":
+    def wrapper(cls: type[T]) -> type[T]:
         return _as_dataclass(
             use_dict=use_dict,
             use_weakref=use_weakref,
@@ -95,71 +130,40 @@ def as_dataclass(
             mapping=mapping,
             iterable=iterable,
             readonly=readonly,
-            module=module,
             fast_new=fast_new,
             rename=rename,
             gc=gc,
-        )(
-            cls
-        )  # type: ignore
+        )(cls)  # type: ignore
 
     return wrapper
 
 
-def exhaust(it: "Iterable[T]", f: "Callable[[T], Any] | None" = None) -> None:
-    deque(it if f is None else map(f, it), 0)
-
-
-def struct_u(s: "str | Struct") -> "Callable[[Callable[[bytes], tuple[Un[Ts]]]], Callable[[bytes], tuple[Un[Ts]]]]":
-    def wrapper(f: "Callable[[bytes], tuple[Un[Ts]]]") -> "Callable[[bytes], tuple[Un[Ts]]]":
+def struct_u(
+    s: str | Struct,
+) -> Callable[[Callable[[bytes], tuple[Un[Ts]]]], Callable[[bytes], tuple[Un[Ts]]]]:
+    def wrapper(
+        f: Callable[[bytes], tuple[Un[Ts]]],
+    ) -> Callable[[bytes], tuple[Un[Ts]]]:
         return Struct(s).unpack if isinstance(s, str) else s.unpack  # type: ignore
 
     return wrapper
 
 
-def struct_p(s: "str | Struct") -> "Callable[[Callable[[Un[Ts]], bytes]], Callable[[Un[Ts]], bytes]]":
-    def wrapper(f: "Callable[[Un[Ts]], bytes]") -> "Callable[[Un[Ts]], bytes]":
+def struct_p(
+    s: str | Struct,
+) -> Callable[[Callable[[Un[Ts]], bytes]], Callable[[Un[Ts]], bytes]]:
+    def wrapper(f: Callable[[Un[Ts]], bytes]) -> Callable[[Un[Ts]], bytes]:
         return Struct(s).pack if isinstance(s, str) else s.pack  # type: ignore
 
     return wrapper
 
 
-def end_decl() -> "NoReturn":
-    raise NotImplementedError()
-
-
-@struct_u("<Q")
-def u8_u(b: bytes) -> "tuple[int]":
-    end_decl()
+def end_decl() -> NoReturn:
+    raise NotImplementedError
 
 
 @struct_u("<I")
-def u4_u(b: bytes) -> "tuple[int]":
-    end_decl()
-
-
-@struct_u("<H")
-def u2_u(b: bytes) -> "tuple[int]":
-    end_decl()
-
-
-@struct_u("<B")
-def u1_u(b: bytes) -> "tuple[int]":
-    end_decl()
-
-
-@struct_u("<f")
-def f4_u(b: bytes) -> "tuple[float]":
-    end_decl()
-
-
-@struct_u("<d")
-def f8_u(b: bytes) -> "tuple[float]":
-    end_decl()
-
-
-@struct_p("<Q")
-def u8_p(i: int) -> bytes:
+def u4_u(b: bytes) -> tuple[int]:
     end_decl()
 
 
@@ -170,19 +174,4 @@ def u4_p(i: int) -> bytes:
 
 @struct_p("<H")
 def u2_p(i: int) -> bytes:
-    end_decl()
-
-
-@struct_p("<B")
-def u1_p(i: int) -> bytes:
-    end_decl()
-
-
-@struct_p("<f")
-def f4_p(i: float) -> bytes:
-    end_decl()
-
-
-@struct_p("<d")
-def f8_p(i: float) -> bytes:
     end_decl()
