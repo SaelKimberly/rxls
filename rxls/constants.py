07a6ca672eb33d476604660a57af28381c733569
SaelKimberly <sael.kimberly@yandex.ru>
2024-01-13 14:49:20 +0500

    Add `constants.py` for better control over used values for types and representation. Note: there is only three base types presented: strings, nulls and numerics. Actually, they presents all data types in ms-excel. Representation of them give us a tree with more datatypes, but these are the roots of this tree. `PREPARED` representation is for chunks of already computed data (allow us to have chunks with unprepared data, useful for lazy operations)

---
+++ rxls/constants.py
@@ -0,0 +1,35 @@
+"""
+Common constants for Excel parsing. TYPE and REPR describes how column chunk must be evaluated.
+
+TYPE_NULL and REPR_PLAIN - Must NOT be used in any part of program. There is no reason for using it.
+
+TYPE_STRING with REPR_PLAIN -> pyarrow.LargeStringArray
+TYPE_STRING with REPR_SHAREDST -> pyarrow.LargeStringArray (taken from shared strings by index)
+TYPE_STRING with REPR_COMPILED -> ready to use pyarrow.Array
+
+TYPE_NUMERIC with REPR_TEMPORAL -> pyarrow.TimestampArray (from float64 to unix timestamp)
+TYPE_NUMERIC with REPR_RKNUMBER -> pyarrow.TimestampArray (rknumber -> float64)
+TYPE_NUMERIC with REPR_TEMPORAL and REPR_RKNUMBER -> pyarrow.TimestampArray (rknumber -> float64 -> unix timestamp)
+TYPE_NUMERIC with REPR_COMPILED -> ready to use pyarrow.Array
+"""
+
+
+__all__ = [
+    "TYPE_NULL",
+    "TYPE_NUMERIC",
+    "TYPE_STRING",
+    "REPR_SHARED",
+    "REPR_TEMPORAL",
+    "REPR_RKNUMBER",
+    "REPR_PREPARED",
+]
+
+TYPE_NULL = 0x00
+TYPE_STRING = 0x10
+TYPE_NUMERIC = 0x20
+
+REPR_PLAIN = 0x00
+REPR_SHARED = REPR_RKNUMBER = 0x01
+REPR_BOOLEAN = 0x02
+REPR_TEMPORAL = 0x04
+REPR_PREPARED = 0x08
