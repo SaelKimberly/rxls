f9938284064abf323e5e8d0125268f243126e14d
SaelKimberly <sael.kimberly@yandex.ru>
2024-01-13 15:13:20 +0500

    Update `rels.py` using Ruff.

--- rxls/rels.py
+++ rxls/rels.py
@@ -1,13 +1,13 @@
-import typing
+# ruff: noqa:D101, D102
+from __future__ import annotations
+
+__all__ = ["Relationship"]
+
+from typing import IO
 
 from pyexpat import ParserCreate
 
 from .core import as_dataclass
-
-if typing.TYPE_CHECKING:
-    from typing import IO
-
-__all__ = ["Relationship"]
 
 RELS_NS = "http://schemas.openxmlformats.org/package/2006/relationships"
 
@@ -19,20 +19,28 @@ class Relationship:
     Target: str
 
     @staticmethod
-    def scan_xml(io: "IO[bytes]") -> "list[Relationship]":
-        rels = []
+    def scan_xml(io: IO[bytes]) -> list[Relationship]:
+        ret = []
 
-        def rs_handler(tag: str, attr: "dict[str, str]"):
-            nonlocal rels
+        def rs_handler(tag: str, attr: dict[str, str]) -> None:
+            nonlocal ret
 
-            if tag == "Relationship":
-                rels.append(Relationship(attr["Id"], attr["Type"].rsplit("/", 1)[-1], attr["Target"]))
+            if tag == "Relationship":  # and attr["Type"].endswith("/worksheet"):
+                ret.append(
+                    Relationship(
+                        attr["Id"],
+                        attr["Type"].rsplit("/", 1)[-1],
+                        attr["Target"],
+                    ),
+                )
 
         parser = ParserCreate()
         parser.StartElementHandler = rs_handler
         parser.ParseFile(io)
 
-        return rels
+        return ret
 
-    def dump_xml(self, io: "IO[bytes]"):
-        io.write(f'<Relationship Id="{self.Id}" Type="{RELS_NS}/{self.Type}" Target="{self.Target}"/>'.encode())
+    def dump_xml(self, io: IO[bytes]) -> None:
+        io.write(
+            f'<Relationship Id="{self.Id}" Type="{RELS_NS}/{self.Type}" Target="{self.Target}"/>'.encode(),
+        )
